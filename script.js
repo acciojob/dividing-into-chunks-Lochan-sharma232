185b446dce8aa86716bcbb9fbb9ce0e069273494
Lochan-sharma232 <lochansharma537@gmail.com>
2025-03-15 16:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
+	function chunkArray[myArray,chunkSize]{
+		let index=0;
+		while(index<myArray.length){
+			results.push(arr.slice(index,index+chunkSize));
+			index+= chunkSize;
+		}
+		return results;
+	}
   // Write your code here
 };
 
-const n = prompt("Enter n: ");
-alert(JSON.stringify(divide(arr, n)));
+
+// const n = prompt("Enter n: ");
+// alert(JSON.stringify(divide(arr, n)));
